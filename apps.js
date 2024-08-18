c1cb188dd621f3b4a18a756aa525f168228ba5e0
systemadminjessesbakery <systemadmin@jessesbakery.com.au>
2024-08-18 17:03:10 +1000

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DriverForm from "./DriverForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Driver Delivery Form</h1>
+      <DriverForm />
+    </div>
+  );
+}
+
+export default App;
